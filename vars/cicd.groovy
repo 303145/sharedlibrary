e859d2a8a7d3a1a7e57f7e20bd703d400f882851
303145 <59477177+303145@users.noreply.github.com>
2024-11-14 12:38:12 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -13,3 +13,10 @@ def deployTomcat(jobname,IP,contextpath)
      sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${IP}:/var/lib/tomcat10/webapps/${contextpath}.war"
 }
 
+def runSelinium(jobname)
+{
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+
+
+
